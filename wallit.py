0e8d550d7ddc20fd450eab8f03c66b2a74ffc9dc
david <david-lky@hotmail.com>
2016-07-23 15:42:24 -0400

    yay

--- wallit.py
+++ wallit.py
@@ -1,12 +1,47 @@
-import sublime, sublimeplugin  
-  
-class Rot13Command(sublimeplugin.TextCommand):  
-    def run(self, view, args):  
-        for region in view.sel():  
-            if not region.empty():  
-                # Get the selected text  
-                s = view.substr(region)  
-                # Transform it via rot13  
-                s = s.encode('rot13')  
-                # Replace the selection with transformed text  
-                view.replace(region, s)  
+import sublime, sublime_plugin
+
+class WallCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        comment = "\n/*\n" \
+                  "TTTTTTTTTTTTTTTTTTTTTTT     RRRRRRRRRRRRRRRRR        UUUUUUUU     UUUUUUUU     MMMMMMMM               MMMMMMMM     PPPPPPPPPPPPPPPPP   \n" \
+                  "T:::::::::::::::::::::T     R::::::::::::::::R       U::::::U     U::::::U     M:::::::M             M:::::::M     P::::::::::::::::P  \n" \
+                  "T:::::::::::::::::::::T     R::::::RRRRRR:::::R      U::::::U     U::::::U     M::::::::M           M::::::::M     P::::::PPPPPP:::::P \n" \
+                  "T:::::TT:::::::TT:::::T     RR:::::R     R:::::R     UU:::::U     U:::::UU     M:::::::::M         M:::::::::M     PP:::::P     P:::::P\n" \
+                  "TTTTTT  T:::::T  TTTTTT       R::::R     R:::::R      U:::::U     U:::::U      M::::::::::M       M::::::::::M       P::::P     P:::::P\n" \
+                  "        T:::::T               R::::R     R:::::R      U:::::D     D:::::U      M:::::::::::M     M:::::::::::M       P::::P     P:::::P\n" \
+                  "        T:::::T               R::::RRRRRR:::::R       U:::::D     D:::::U      M:::::::M::::M   M::::M:::::::M       P::::PPPPPP:::::P \n" \
+                  "        T:::::T               R:::::::::::::RR        U:::::D     D:::::U      M::::::M M::::M M::::M M::::::M       P:::::::::::::PP  \n" \
+                  "        T:::::T               R::::RRRRRR:::::R       U:::::D     D:::::U      M::::::M  M::::M::::M  M::::::M       P::::PPPPPPPPP    \n" \
+                  "        T:::::T               R::::R     R:::::R      U:::::D     D:::::U      M::::::M   M:::::::M   M::::::M       P::::P            \n" \
+                  "        T:::::T               R::::R     R:::::R      U:::::D     D:::::U      M::::::M    M:::::M    M::::::M       P::::P            \n" \
+                  "        T:::::T               R::::R     R:::::R      U::::::U   U::::::U      M::::::M     MMMMM     M::::::M       P::::P            \n" \
+                  "      TT:::::::TT           RR:::::R     R:::::R      U:::::::UUU:::::::U      M::::::M               M::::::M     PP::::::PP          \n" \
+                  "      T:::::::::T           R::::::R     R:::::R       UU:::::::::::::UU       M::::::M               M::::::M     P::::::::P          \n" \
+                  "      T:::::::::T           R::::::R     R:::::R         UU:::::::::UU         M::::::M               M::::::M     P::::::::P          \n" \
+                  "      TTTTTTTTTTT           RRRRRRRR     RRRRRRR           UUUUUUUUU           MMMMMMMM               MMMMMMMM     PPPPPPPPPP          \n" \
+                  "*/\n"
+        view = self.view
+        for region in self.view.sel():
+            if not region.empty():
+                syntax = view.settings().get('syntax')
+                if str(syntax).lower() == "python":
+                    comment = "\n\"\"\"\n" \
+                              "TTTTTTTTTTTTTTTTTTTTTTT     RRRRRRRRRRRRRRRRR        UUUUUUUU     UUUUUUUU     MMMMMMMM               MMMMMMMM     PPPPPPPPPPPPPPPPP   \n" \
+                              "T:::::::::::::::::::::T     R::::::::::::::::R       U::::::U     U::::::U     M:::::::M             M:::::::M     P::::::::::::::::P  \n" \
+                              "T:::::::::::::::::::::T     R::::::RRRRRR:::::R      U::::::U     U::::::U     M::::::::M           M::::::::M     P::::::PPPPPP:::::P \n" \
+                              "T:::::TT:::::::TT:::::T     RR:::::R     R:::::R     UU:::::U     U:::::UU     M:::::::::M         M:::::::::M     PP:::::P     P:::::P\n" \
+                              "TTTTTT  T:::::T  TTTTTT       R::::R     R:::::R      U:::::U     U:::::U      M::::::::::M       M::::::::::M       P::::P     P:::::P\n" \
+                              "        T:::::T               R::::R     R:::::R      U:::::D     D:::::U      M:::::::::::M     M:::::::::::M       P::::P     P:::::P\n" \
+                              "        T:::::T               R::::RRRRRR:::::R       U:::::D     D:::::U      M:::::::M::::M   M::::M:::::::M       P::::PPPPPP:::::P \n" \
+                              "        T:::::T               R:::::::::::::RR        U:::::D     D:::::U      M::::::M M::::M M::::M M::::::M       P:::::::::::::PP  \n" \
+                              "        T:::::T               R::::RRRRRR:::::R       U:::::D     D:::::U      M::::::M  M::::M::::M  M::::::M       P::::PPPPPPPPP    \n" \
+                              "        T:::::T               R::::R     R:::::R      U:::::D     D:::::U      M::::::M   M:::::::M   M::::::M       P::::P            \n" \
+                              "        T:::::T               R::::R     R:::::R      U:::::D     D:::::U      M::::::M    M:::::M    M::::::M       P::::P            \n" \
+                              "        T:::::T               R::::R     R:::::R      U::::::U   U::::::U      M::::::M     MMMMM     M::::::M       P::::P            \n" \
+                              "      TT:::::::TT           RR:::::R     R:::::R      U:::::::UUU:::::::U      M::::::M               M::::::M     PP::::::PP          \n" \
+                              "      T:::::::::T           R::::::R     R:::::R       UU:::::::::::::UU       M::::::M               M::::::M     P::::::::P          \n" \
+                              "      T:::::::::T           R::::::R     R:::::R         UU:::::::::UU         M::::::M               M::::::M     P::::::::P          \n" \
+                              "      TTTTTTTTTTT           RRRRRRRR     RRRRRRR           UUUUUUUUU           MMMMMMMM               MMMMMMMM     PPPPPPPPPP          \n" \
+                              "\"\"\n"
+                view.insert(edit, region.begin(), comment)
+                view.insert(edit, len(comment)+region.end(), comment)
